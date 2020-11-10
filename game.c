b8e8eb774144606aae246f6bc904c37fccd5deb1
zaryanz <aryanx404@gmail.com>
2020-11-10 08:54:04 +0530

    first commit

---
+++ game.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include <stdlib.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+typedef struct gameType {
+    SDL_Event event;
+    int loopDone;
+} gameType;
+
+gameType game;
+
+void input()
+{
+    while(SDL_PollEvent(&game.event) != 0) {
+        switch(game.event.type) {
+            case SDL_QUIT:
+                game.loopDone=1;
+        }
+    }
+}
+
+int main() 
+{
+    SDL_Window *window;
+
+    if(SDL_Init(SDL_INIT_VIDEO)!= 0) {
+        fprintf(stderr, "Could not initialize SDL Video: %s\n", SDL_GetError());
+    }
+
+    window = SDL_CreateWindow("hello", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if(window == NULL) {
+        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+        exit(1);
+    }
+    while(!game.loopDone) {
+        input();
+    }
+    return 0;
+}
